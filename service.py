4657c3711ecb0369a0ecd0526a54101ed8841344
R0mNavarro0 <romainnavarro.pro@gmail.com>
2025-08-28 21:55:53 +0200

    service / query correction

--- service.py
+++ service.py
@@ -5,21 +5,21 @@ from pydantic import BaseModel, Field
 
 class EnergyInput(BaseModel):
     PropertyGFATotal: float = Field(..., example=10000)
-    PropertyGFABuilding_s: float = Field(..., example=8000)
+    PropertyGFABuilding_s: float = Field(..., example=8000, alias="PropertyGFABuilding(s)")
     LargestPropertyUseTypeGFA: float = Field(..., example=6000)
     Electricity_Part: float = Field(..., example=70, le=100, ge=0)
     NaturalGas_Part: float = Field(..., example=30, le=100, ge=0)
     BuildingAge: float = Field(..., example=20)
 
 @bentoml.service
-class EnerygyCO2Service:
+class EnergyCO2Service:
     def __init__(self) -> None:
-        self.energy_runner = bentoml.sklearn.load_model("energy_consumption_model_light:latest")
-        self.co2_runner = bentoml.sklearn.load_model("c02_emissions_model:latest")
+        self.energy_runner = bentoml.sklearn.load_model("energy_consumption_model_light:epy5jnmee2j44aav")
+        self.co2_runner = bentoml.sklearn.load_model("c02_emissions_model:aom7at4csoiheaav")
 
     @bentoml.api
     def energy_predict(self, data: dict) -> dict:
-        validated_data = EnergyInput(**data).dict()
+        validated_data = EnergyInput(**data).dict(by_alias=True)
         data_log = {k: np.log1p(v) for k, v in validated_data.items()}
         df = pd.DataFrame([data_log])
         y_pred_energy = self.energy_runner.predict(df)
